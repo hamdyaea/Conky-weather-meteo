f2621871b3442a2cf4b2b474b0912d1fbcdaeb0f
hamdyaea <hamdy.aea@protonmail.com>
2019-11-19 20:43:27 +0100

    code beauty

--- speedtest.bash
+++ speedtest.bash
@@ -7,17 +7,17 @@ wget -q --spider https://raw.githubusercontent.com/sivel/speedtest-cli/master/sp
 
 if [ $? -eq 0  ]; then
 
-speedtest="$(curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -)"
-echo $speedtest > speedresult
-downspeed="$(cat speedresult| cut -d ':' -f 3 | cut -c -13)"
-upspeed="$(cat speedresult | cut -d ':' -f 4)"
-pingresult="$(cat speedresult| cut -d ':' -f 2 | cut -c -7)"
+    speedtest="$(curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -)"
+    echo $speedtest > speedresult
+    downspeed="$(cat speedresult| cut -d ':' -f 3 | cut -c -13)"
+    upspeed="$(cat speedresult | cut -d ':' -f 4)"
+    pingresult="$(cat speedresult| cut -d ':' -f 2 | cut -c -7)"
 
 else
 
-downspeed=" 0"
-upspeed=" 0"
-pingresult=" 0"
+    downspeed=" 0"
+    upspeed=" 0"
+    pingresult=" 0"
 fi
 
 echo "Down"$downspeed "Up"$upspeed "Ping"$pingresult
